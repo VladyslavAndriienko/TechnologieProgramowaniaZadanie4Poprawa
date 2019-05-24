c0aa059fe285904df62a65829f0d0aff0c9e442a
Vladyslav Mazur <199185@edu.p.lodz.pl>
2019-05-24 22:49:28 +0300

    [TP-Zadanie 4] Dodanie EventHandlera

--- Zadanie_4_WPF_v2/ViewModels/Command.cs
+++ Zadanie_4_WPF_v2/ViewModels/Command.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Zadanie_4_WPF_v2.ViewModels
 {
-    class Command
+    class Command : ICommand
     {
+        private readonly Func<Boolean> _canExecute;
+        private readonly Action _execute;
+
+        public Command(Action execute)
+			: this(execute, null)
+		{
+        }
+
+        public Command(Action execute, Func<Boolean> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public Boolean CanExecute(Object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(Object parameter)
+        {
+            _execute();
+        }
     }
 }
